761f13432d82ee041f9d8b1148e6a6022de9526a
Dragan <d.novakovic1810@yahoo.com>
2017-06-29 15:52:18 +0200

    12/40

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const storeControler = require('../controllers/storeControler');
+const {catchErrors} = require('../handlers/errorHandlers');
 
 // Do work here
-router.get('/', (req, res) => {
-  res.render('hello');
-});
+router.get('/', catchErrors(storeControler.getStores));
+router.get('/stores', catchErrors(storeControler.getStores));
+router.get('/add', storeControler.addStore);
+router.post('/add',catchErrors(storeControler.createStore));
 
 module.exports = router;
